1d69864eb6f5f43e92d64ac0a1f2eb20252787ae
Tvv95 <truvladislav@gmail.com>
2019-08-11 18:40:24 +0300

    initialization

---
+++ src/bin/brain-even.js
@@ -0,0 +1,7 @@
+#! /usr/bin/node
+import evenGame from '../index.js';
+
+console.log('Welcome to the Brain Games!');
+console.log('Answer "yes" if number even otherwise answer "no".');
+console.log();
+evenGame();
