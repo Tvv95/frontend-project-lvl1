67967b5bfad78908771816162790a2e45ff3f92a
Tvv95 <truvladislav@gmail.com>
2019-08-11 18:39:14 +0300

    changed index.js

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,6 +1,7 @@
 #! /usr/bin/node
-import { askName } from '../index.js';
+import askName from '../index.js';
 
 console.log('Welcome to the Brain Games!');
+console.log('Answer "yes" if number even otherwise answer "no".');
 console.log();
 askName();
