42a213b8af5df21adeb00e939764b75ce8d0212f
Tvv95 <truvladislav@gmail.com>
2019-08-18 23:34:41 +0300

    delete excess functions

--- src/bin/brain-gcd.js
+++ src/bin/brain-gcd.js
@@ -1,9 +1,7 @@
-#! /usr/bin/node
+#! /usr/bin/env node
 import {
-  printGreeting, askName, coreGames,
+  coreGames,
 } from '../index';
-import gcdGame from '../games/gcd-core';
+import gcdGame from '../games/gcd';
 
-printGreeting('Find the greatest common divisor of given numbers.');
-askName();
 coreGames(gcdGame);
