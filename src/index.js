a20ea280c57e739b94397a4e18837d25619f903a
Tvv95 <truvladislav@gmail.com>
2019-08-18 23:41:51 +0300

    combin functions

--- src/index.js
+++ src/index.js
@@ -1,38 +1,38 @@
 import readlineSync from 'readline-sync';
 
-export const printGreeting = (greeting) => {
-  console.log('Welcome to the Brain Games!');
-  console.log(greeting);
-  console.log();
+export const randomInteger = (min, max) => {
+  const randomNumber = min + Math.random() * (max + 1 - min);
+  return Math.floor(randomNumber);
 };
-let userName;
 
-export const askName = () => {
-  userName = readlineSync.question('May I have your name? ');
+export const coreGames = (game) => {
+  let gameInfo = game();
+  console.log('Welcome to the Brain Games!');
+  console.log(gameInfo[2]);
+  console.log();
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log();
-};
-export const randomInteger = (min, max) => {
-  const randNumber = min + Math.random() * (max + 1 - min);
-  return Math.floor(randNumber);
-};
-let counter = 0;
-
-export const coreGames = (gameFunc) => {
-  const gameInfo = gameFunc();
-  console.log(`Question: ${gameInfo[0]}`);
-  const answer = readlineSync.question('Your answer: ');
-  const corrAnswer = gameInfo[1];
-  if (answer === corrAnswer) {
-    console.log('Correct!');
-    counter += 1;
-    if (counter === 3) {
-      console.log(`Congratulations, ${userName}!`);
-    } else {
-      coreGames(gameFunc);
+  let counter = 0;
+  const core = () => {
+    if (counter > 0) {
+      gameInfo = game();
     }
-  } else {
-    console.log(`"${answer}" is wrong answer ;(. Correct answer was "${corrAnswer}".`);
-    console.log(`Let's try again, ${userName}!`);
-  }
+    console.log(`Question: ${gameInfo[0]}`);
+    const answer = readlineSync.question('Your answer: ');
+    const correctAnswer = gameInfo[1];
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+      counter += 1;
+      if (counter === 3) {
+        console.log(`Congratulations, ${userName}!`);
+      } else {
+        core();
+      }
+    } else {
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
+      console.log(`Let's try again, ${userName}!`);
+    }
+  };
+  core();
 };
