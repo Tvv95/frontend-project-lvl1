01e239610b5155ce617e6099a0351a95a12e1d43
Tvv95 <truvladislav@gmail.com>
2019-08-18 23:35:16 +0300

    delete excess functions

--- src/bin/brain-prime.js
+++ src/bin/brain-prime.js
@@ -1,9 +1,7 @@
-#! /usr/bin/node
+#! /usr/bin/env node
 import {
-  printGreeting, askName, coreGames,
+  coreGames,
 } from '../index';
-import primeGame from '../games/prime-core';
+import primeGame from '../games/prime';
 
-printGreeting('Answer "yes" if given number is prime. Otherwise answer "no".');
-askName();
 coreGames(primeGame);
