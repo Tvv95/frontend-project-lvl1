b4a59c89c1e43ada32a922428d3c79705adc0d49
Tvv95 <truvladislav@gmail.com>
2019-08-26 10:23:59 +0300

    changed core function system

--- src/index.js
+++ src/index.js
@@ -1,36 +1,27 @@
 import readlineSync from 'readline-sync';
 
-export const randomInteger = (min, max) => {
-  const randomNumber = min + Math.random() * (max + 1 - min);
-  return Math.floor(randomNumber);
-};
-
-export const coreGames = (game) => {
-  let gameInfo = game();
+export default (getGameInformation, task) => {
   console.log('Welcome to the Brain Games!');
-  console.log(`${gameInfo[2]}\n`);
+  console.log(`${task}\n`);
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!\n`);
-  let counter = 0;
-  const core = () => {
-    if (counter > 0) {
-      gameInfo = game();
-    }
-    console.log(`Question: ${gameInfo[0]}`);
+  const gameRounds = 3;
+  let correctAnswers = 0;
+  for (let i = 0; i < gameRounds; i += 1) {
+    const gameInformation = getGameInformation();
+    console.log(`Question: ${gameInformation[0]}`);
     const answer = readlineSync.question('Your answer: ');
-    const correctAnswer = gameInfo[1];
+    const correctAnswer = gameInformation[1];
     if (answer === correctAnswer) {
       console.log('Correct!');
-      counter += 1;
-      if (counter === 3) {
+      correctAnswers += 1;
+      if (correctAnswers === 3) {
         console.log(`Congratulations, ${userName}!`);
-      } else {
-        core();
       }
     } else {
       console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
       console.log(`Let's try again, ${userName}!`);
+      i = gameRounds;
     }
-  };
-  core();
+  }
 };
