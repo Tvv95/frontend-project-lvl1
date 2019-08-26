167b767dc8cba7ef0c3a36550d2522efea4c39f2
Tvv95 <truvladislav@gmail.com>
2019-08-26 10:21:55 +0300

    added getGameInformation

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,29 +1,21 @@
-import { randomInteger } from '..';
+import startCoreGame from '..';
+import randomInteger from '../utils';
 
-const searchGCD = (first, second) => {
+const searchGcd = (first, second) => {
   if (first % second === 0) {
     return second;
   }
-  return searchGCD(second, first % second);
+  return searchGcd(second, first % second);
 };
+const task = 'Find the greatest common divisor of given numbers.';
 
-export default () => {
-  const task = 'Find the greatest common divisor of given numbers.';
-  const gameInformation = [];
-  const firstRandomNumber = randomInteger(1, 100);
-  const secondRandomNumber = randomInteger(1, 100);
-  gameInformation.push(`${firstRandomNumber} ${secondRandomNumber}`);
-  let max;
-  let min;
-  if (firstRandomNumber >= secondRandomNumber) {
-    max = firstRandomNumber;
-    min = secondRandomNumber;
-  } else {
-    max = secondRandomNumber;
-    min = firstRandomNumber;
-  }
-  const correctAnswer = searchGCD(max, min);
-  gameInformation.push(String(correctAnswer));
-  gameInformation.push(task);
+const getGameInformation = () => {
+  const firstValue = randomInteger(1, 100);
+  const secondValue = randomInteger(1, 100);
+  const correctAnswer = searchGcd(Math.max(firstValue, secondValue),
+    Math.min(firstValue, secondValue));
+  const gameInformation = [`${firstValue} ${secondValue}`, String(correctAnswer)];
   return gameInformation;
 };
+
+export default () => startCoreGame(getGameInformation, task);
