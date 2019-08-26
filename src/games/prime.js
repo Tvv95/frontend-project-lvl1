9fb526419000c27be87f67e615bd456a0240e42a
Tvv95 <truvladislav@gmail.com>
2019-08-26 10:22:12 +0300

    added getGameInformation

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,25 +1,27 @@
-import { randomInteger } from '..';
+import startCoreGame from '..';
+import randomInteger from '../utils';
 
-const searchSimple = (number) => {
+const isSimple = (value) => {
+  if (value < 2) {
+    return false;
+  }
   const iter = (counter) => {
-    if (counter === 1) {
+    if (counter === Math.round(Math.sqrt(value))) {
       return true;
     }
-    if (number % counter === 0) {
+    if (value % counter === 0) {
       return false;
     }
-    return iter(counter - 1);
+    return iter(counter + 1);
   };
-  return iter(Math.round(Math.sqrt(number)));
+  return iter(2);
 };
-
-export default () => {
-  const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-  const gameInformation = [];
+const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const getGameInformation = () => {
   const question = randomInteger(1, 100);
-  gameInformation.push(question);
-  const correctAnswer = searchSimple(question) ? 'yes' : 'no';
-  gameInformation.push(correctAnswer);
-  gameInformation.push(task);
+  const correctAnswer = isSimple(question) ? 'yes' : 'no';
+  const gameInformation = [question, correctAnswer];
   return gameInformation;
 };
+
+export default () => startCoreGame(getGameInformation, task);
