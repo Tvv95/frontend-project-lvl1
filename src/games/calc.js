7b68e1294f0f0a557b97bb5831f048665a9a68b6
Tvv95 <truvladislav@gmail.com>
2019-08-29 09:37:54 +0300

    name change

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,24 +4,26 @@ import randomInteger from '../utils';
 const task = 'What is the result of the expression?';
 const operations = ['+', '-', '*'];
 
-const getGameInformation = () => {
+const getGameData = () => {
   const firstValue = randomInteger(1, 100);
   const secondValue = randomInteger(1, 100);
   const currentOperation = randomInteger(0, operations.length - 1);
-  const gameInformation = [`${firstValue} ${operations[currentOperation]} ${secondValue}`];
-  switch (currentOperation) {
-    case 0:
-      gameInformation.push(String(firstValue + secondValue));
+  const question = [`${firstValue} ${operations[currentOperation]} ${secondValue}`];
+  let correctAnswer;
+  switch (operations[currentOperation]) {
+    case operations[0]:
+      correctAnswer = (String(firstValue + secondValue));
       break;
-    case 1:
-      gameInformation.push(String(firstValue - secondValue));
+    case operations[1]:
+      correctAnswer = (String(firstValue - secondValue));
       break;
-    case 2:
-      gameInformation.push(String(firstValue * secondValue));
+    case operations[2]:
+      correctAnswer = (String(firstValue * secondValue));
       break;
     default:
       break;
   }
-  return gameInformation;
+  const gameData = [question, correctAnswer];
+  return gameData;
 };
-export default () => startCoreGame(getGameInformation, task);
+export default () => startCoreGame(getGameData, task);
