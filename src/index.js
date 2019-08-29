d83ef5a8e2d8d4d85dbf1bd4aa26f74a72c863d5
Tvv95 <truvladislav@gmail.com>
2019-08-29 09:41:59 +0300

    change names, counter rounds

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
 import readlineSync from 'readline-sync';
 
-export default (getGameInformation, task) => {
+const roundsCount = 3;
+
+export default (getGameData, task) => {
   console.log('Welcome to the Brain Games!');
   console.log(`${task}\n`);
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!\n`);
-  const gameRounds = 3;
-  let correctAnswers = 0;
-  for (let i = 0; i < gameRounds; i += 1) {
-    const gameInformation = getGameInformation();
-    console.log(`Question: ${gameInformation[0]}`);
+  for (let i = 0; i < roundsCount; i += 1) {
+    const gameData = getGameData();
+    console.log(`Question: ${gameData[0]}`);
     const answer = readlineSync.question('Your answer: ');
-    const correctAnswer = gameInformation[1];
+    const correctAnswer = gameData[1];
     if (answer === correctAnswer) {
       console.log('Correct!');
-      correctAnswers += 1;
-      if (correctAnswers === 3) {
-        console.log(`Congratulations, ${userName}!`);
-      }
     } else {
       console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
       console.log(`Let's try again, ${userName}!`);
-      i = gameRounds;
+      return;
     }
   }
+  console.log(`Congratulations, ${userName}!`);
 };
